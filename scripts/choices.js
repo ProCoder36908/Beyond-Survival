ccac1acd139fc53b103bc23ed15ebd4253da6b97
ProGrammer_365 <133169817+ProCoder36908@users.noreply.github.com>
2023-12-31 14:18:22 -0500

    choices

---
+++ scripts/choices.js
@@ -0,0 +1,16 @@
+function showChoices() {
+    document.getElementById('startButton').style.display = 'none';
+    document.getElementById('choices').style.display = 'block';
+}
+
+function findPlane() {
+    alert("You chose to find the plane. Good luck on your adventure!");
+}
+
+function findMaterials() {
+    alert("You chose to find materials. May you gather what you need for your journey!");
+}
+
+function findHumans() {
+    alert("You chose to find humans. Interacting with others can be key to survival!");
+}
